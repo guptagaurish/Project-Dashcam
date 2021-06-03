7ffb477628dd13f5a10d815d0832c92e26b4597f
guptagaurish <67861590+guptagaurish@users.noreply.github.com>
2021-06-03 12:23:22 +0530

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,12 +7,13 @@ from .views import *
 
 urlpatterns = [
     path('myapp/', views.Login_get),
-    path('myapp/<imei>', views.Alarmdata_get),
-    path('myapp/<imei>', views.Locdata_get),
+    path('myapp/alarm/<imei>', views.Alarmdata_get),
+    path('myapp/location/<imei>', views.Locdata_get),
     path('myapp/upload/',views.upload_video,name='upload'),
     path('videos/',display,name='videos'),
     path('myapp/command', views.Command_get.as_view()),
     path('myapp/command-response', views.Commandresponse_get.as_view()),
+    path('myapp/filter-alarm/',views.fetch_alarmby_type)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
